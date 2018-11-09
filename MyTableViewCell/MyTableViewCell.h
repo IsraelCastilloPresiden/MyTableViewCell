c80edf93f1f55abd15188a51950a9083b6859726
Israel Gtz <igtz.castillo@gmail.com>
2018-11-08 21:34:41 -0600

    Initial Commit

---
+++ MyTableViewCell/MyTableViewCell.h
@@ -0,0 +1,19 @@
+//
+//  MyTableViewCell.h
+//  MyTableViewCell
+//
+//  Created by Israel Gtz on 08/11/18.
+//  Copyright © 2018 Israel Gtz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MyTableViewCell.
+FOUNDATION_EXPORT double MyTableViewCellVersionNumber;
+
+//! Project version string for MyTableViewCell.
+FOUNDATION_EXPORT const unsigned char MyTableViewCellVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyTableViewCell/PublicHeader.h>
+
+
